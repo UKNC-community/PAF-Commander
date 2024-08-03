81740ca8a2d28c6de926083f253502473ca2d38b
Петросян Александр Фридрихович <afpetrosyan@beeline.ru>
2024-08-03 17:33:26 +0300

    hmm. start does not work.
    r pc.exe starts it but with broken device name and no panels

--- up.cmd
+++ up.cmd
@@ -1,9 +1,8 @@
-:rt11dsk d mz80_PC.dsk PC.SYS
+rt11dsk d mz80_PC.dsk PC.SYS
 rt11dsk d mz80_PC.dsk PC.EXE
-:rt11dsk d mz80_PC.dsk PC.SAV
+rt11dsk d mz80_PC.dsk PC.SAV
 :rt11dsk d mz80_PC.dsk BANK.FNT
-rt11dsk d mz80_PC.dsk PAF*.SAV
 
 rt11dsk a mz80_PC.dsk PC.EXE
-:rt11dsk a mz80_PC.dsk PC.SAV
-:rt11dsk a mz80_PC.dsk PC.SYS
+rt11dsk a mz80_PC.dsk PC.SAV
+rt11dsk a mz80_PC.dsk PC.SYS
