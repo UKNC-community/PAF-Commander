3cbadb8f9f2c437d0d430dbab8cfe6ecbad5eeb8
Петросян Александр Фридрихович <afpetrosyan@beeline.ru>
2024-08-03 18:08:12 +0300

    .ini file was to blame. Thanks, Alexey Kisly
    Alexey Kisly, [3 авг. 2024 г., 17:43:06]:
    Ещё надо удалять PC.INI. Как я понял там записаны не только значения сохраняемых переменных, но и их адреса в памяти. Когда сборка другая, то и адреса другие. Я когда сделал без защиты, то у меня она вылетала по TRAP4. Прошёлся отладчиком в UKNCBTL, вылетала при чтении INI-файла. Удалил, всё нормально запустилось.

--- up.cmd
+++ up.cmd
@@ -2,7 +2,7 @@ rt11dsk d mz80_PC.dsk PC.SYS
 rt11dsk d mz80_PC.dsk PC.EXE
 rt11dsk d mz80_PC.dsk PC.SAV
 :rt11dsk d mz80_PC.dsk BANK.FNT
+rt11dsk d mz80_PC.dsk PC.INI
 
-rt11dsk a mz80_PC.dsk PC.EXE
 rt11dsk a mz80_PC.dsk PC.SAV
 rt11dsk a mz80_PC.dsk PC.SYS
